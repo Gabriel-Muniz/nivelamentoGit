e390fc181d134f2f509b223adf16c92bb030114a
Gabriel Muniz <gabriel.muniz1004@gmail.com>
2021-01-29 19:03:37 -0300

    Acrescentada mensagem de bom dia.

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,7 +16,9 @@ public class Program {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       System.out.println("Bom dia mundo!");
+       
     }
 
 }
