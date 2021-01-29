3b2fae7ca5b291bb6191f7fcb9f3f8d1e1414ff6
Gabriel Muniz <gabriel.muniz1004@gmail.com>
2021-01-29 19:20:45 -0300

    Mensagem de boa noite

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,9 @@ public class Program {
      */
     public static void main(String[] args) {
         
-       System.out.println("Bom dia mundo!");
+       System.out.println("Bom dia!");
+       System.out.println("Bom tarde");  
+       System.out.println("Boa noite");  
        
     }
 
